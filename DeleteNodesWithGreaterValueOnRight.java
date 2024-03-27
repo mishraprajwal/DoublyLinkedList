8f10f54ea8ca240b5a79f961f7c32232c63587eb
mishraprajwal <prajwalm882@gmail.com>
2024-03-27 10:50:52 -0400

    Updated DeleteNodesWithGreaterValueOnRight

--- DeleteNodesWithGreaterValueOnRight.java
+++ DeleteNodesWithGreaterValueOnRight.java
@@ -1,33 +1,34 @@
 public class DeleteNodesWithGreaterValueOnRight {
-    public void deleteNodesWithGreaterValueOnRight (CreateDLL list) {
+    public void deleteNodesWithGreaterValueOnRight(CreateDLL list) {
+        // Check if the list is empty or has only one element
         if (list.head == null || list.head.next == null) {
-            System.out.println("The list is empty.");
-        } else {
-            Node curr = list.head;
-            while (curr.next != null) {
-                curr = curr.next;
-            }
-            Node tail = curr;
-            curr = curr.prev;
-            while (curr != null) {
-                if (curr.data < tail.data) {
-                    Node temp = curr.prev;
-                    if (temp != null) {
-                        temp.next = curr.next;
-                    }
-
-                    if (curr.next != null) {
-                        curr.next.prev = temp;
-                    }
-                } else {
-                    tail = curr;
-                }
-                curr = curr.prev;
-            }
-
-            while (list.head != null && list.head.prev != null) {
-                list.head = list.head.prev;
-            }
+            return;
         }
-    } 
+        
+        Node curr = list.head;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        Node tail = curr;
+        curr = curr.prev;
+        
+        while (curr != null) {
+            if (curr.data < tail.data) {
+                // Deletion needed
+                if (curr.prev != null) {
+                    curr.prev.next = curr.next;
+                }
+                if (curr.next != null) {
+                    curr.next.prev = curr.prev;
+                }
+                if (curr == list.head) {
+                    list.head = curr.next;
+                }
+            } else {
+                tail = curr;
+            }
+            curr = curr.prev;
+        }
+    }
 }
